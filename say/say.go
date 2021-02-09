59ae5f9fde1a381492b996985e063cfb428ede90
alxeyil <aleksey.ilichenko@yandex.ru>
2021-02-09 19:34:51 +0300

    ok

---
+++ say/say.go
@@ -0,0 +1,9 @@
+package say
+
+import (
+	"fmt"
+)
+
+func sayGo() {
+	fmt.Printf("hello from second pkg")
+}
---
+++ test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/alexeyil/go-test/say/say"
+)
+
+func main() {
+	fmt.Printf("Hello, world")
+	say.sayGo()
+}
